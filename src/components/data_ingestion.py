230a56dab228322b24e6b58be0bb261ff19c49f2
Tohubar <hossaintohubar@gmail.com>
2025-09-05 11:23:49 +0600

    Data Tansformation i added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join("artifacts", "train.csv")
@@ -41,4 +44,7 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_path, test_path = obj.initiate_data_ingestion()
+    
+    data_transfromation = DataTransformation()
+    data_transfromation.initiate_data_transformation(train_path=train_path, test_path= test_path)
