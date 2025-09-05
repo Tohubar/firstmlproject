7189944b17c6a9b80ce55bf441e5db49801b5f8c
Tohubar <hossaintohubar@gmail.com>
2025-09-05 14:02:30 +0600

    Model Training added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -47,4 +48,6 @@ if __name__ == "__main__":
     train_path, test_path = obj.initiate_data_ingestion()
     
     data_transfromation = DataTransformation()
-    data_transfromation.initiate_data_transformation(train_path=train_path, test_path= test_path)
+    train_arr, test_arr, _ = data_transfromation.initiate_data_transformation(train_path=train_path, test_path= test_path)
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_array= train_arr, test_array= test_arr))
